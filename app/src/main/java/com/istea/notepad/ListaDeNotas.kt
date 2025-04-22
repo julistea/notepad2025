023e6e1ce6b0dd1c44173b5ad73cd51afe0951eb
ErickIstea <ericksilva1988@gmail.com>
2025-04-22 20:24:07 -0300

    agrego cards para mostrar la lista

--- app/src/main/java/com/istea/notepad/ListaDeNotas.kt
+++ app/src/main/java/com/istea/notepad/ListaDeNotas.kt
@@ -1,9 +1,18 @@
 package com.istea.notepad
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -15,6 +24,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.istea.notepad.ui.theme.NotePadTheme
@@ -51,10 +61,36 @@ fun ListaDeNotasView(
             )
         }
     ) { padding ->
-        Column(
-            modifier = Modifier.padding(padding)
+        LazyColumn(
+            modifier = Modifier.padding(padding),
+            contentPadding = PaddingValues(15.dp)
         ) {
-            Text("Soy una lista de notas ${listaDeNotas.size}")
+            items(listaDeNotas) { nota ->
+                ElevatedCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(horizontal = 15.dp)
+                                .padding(top = 10.dp),
+                            text = nota.titulo,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(horizontal = 15.dp)
+                                .padding(bottom = 10.dp),
+                            text = nota.texto,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+                Spacer(modifier.height(15.dp))
+            }
         }
     }
 }
